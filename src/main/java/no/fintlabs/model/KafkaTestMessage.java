78558042507dd748b57556d9ca8f76230b2481b8
Nils H Opsahl <nils.hakon.opsahl@vigoiks.no>
2023-04-28 12:05:36 +0200

    Change to Springboot 2.7.10 Change @Data

--- src/main/java/no/fintlabs/model/KafkaTestMessage.java
+++ src/main/java/no/fintlabs/model/KafkaTestMessage.java
@@ -2,11 +2,12 @@ package no.fintlabs.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.Map;
 
 @Builder
-@Data
+@Getter
 public class KafkaTestMessage {
     private final String sysId;
     private final String tableName;
